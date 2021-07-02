28fa43db14b292529e7ee7e12feae2a748c11bad
Waihon Yew <yewwaihon@gmail.com>
2021-07-02 14:55:21 +0800

    Convert astronaut data into a Swift struct

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Waihon Yew on 02/07/2021.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
